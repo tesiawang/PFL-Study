ecb1716849317c8537245e8aa4e7973af7c3c5ea
Tianxin <tianxin.wang@sjtu.edu.cn>
2024-02-15 10:13:20 +0000

    refresh

--- plot.py
+++ plot.py
@@ -23,8 +23,8 @@ with open('recorder_weighted_new_obj4.pkl', 'rb') as f:
     mean_per_acc4 = recorder['mean_personalized_acc']
     mean_gen_acc4 = recorder['mean_generalized_acc']
 
-print(mean_per_acc4[65:69])
-print(mean_per_acc0[55:59])
+# print(mean_per_acc4[65:69])
+# print(mean_per_acc0[55:59])
 # with open('recorder_weighted_new_obj2.pkl', 'rb') as f:
 #     recorder = pickle.load(f)
 #     mean_per_acc = recorder['mean_personalized_acc']
@@ -32,10 +32,10 @@ print(mean_per_acc0[55:59])
 
 # Plot
 plt.figure(1)
-plt.plot(mean_per_acc0, label='Personalized Accuracy (oringinal objective with weighted initial)')
+plt.plot(np.array(mean_per_acc0[0:59]), label='Personalized Accuracy (existing objective)')
 # plt.plot(mean_per_acc1, label='Personalized Accuracy (only the first order)')
 # plt.plot(mean_per_acc2, label='Personalized Accuracy (new loss)')
-plt.plot(mean_per_acc4, label='Personalized Accuracy (new error bound)')
+plt.plot(mean_per_acc4[0:59], label='Personalized Accuracy (new error bound)')
 # plt.plot(mean_per_acc, label='Personalized Accuracy (new loss with weighted initial)')
 
 plt.legend()
@@ -44,10 +44,10 @@ plt.ylabel('Personalized Accuracy')
 
 
 plt.figure(2)
-plt.plot(mean_gen_acc0, label='Generalized Accuracy (oringinal objective with weighted initial)')
+plt.plot(mean_gen_acc0[0:59], label='Generalized Accuracy (existing objective)')
 # plt.plot(mean_gen_acc1, label='Generalized Accuracy (only the first order)')
 # plt.plot(mean_gen_acc2, label='Generalized Accuracy (new loss)')
-plt.plot(mean_gen_acc4, label='Generalized Accuracy (new error bound)')
+plt.plot(mean_gen_acc4[0:59], label='Generalized Accuracy (new error bound)')
 # plt.plot(mean_gen_acc, label='Generalized Accuracy (new loss with weighted initial)')
 plt.legend()
 plt.xlabel('Communication Round')
